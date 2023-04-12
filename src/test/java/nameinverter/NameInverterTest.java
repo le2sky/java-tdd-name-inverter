7d2823f604b0a293f16e439543d3089eb9122e42
leesky <leehaneul0623@gmail.com>
2023-04-13 04:54:30 +0900

    test : givendpost nominals exists post nominals stays at end

--- src/test/java/nameinverter/NameInverterTest.java
+++ src/test/java/nameinverter/NameInverterTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 public class NameInverterTest {
@@ -19,6 +20,8 @@ public class NameInverterTest {
         assertThat(invert("first     last")).isEqualTo("last, first");
         assertThat(invert("Mr. first last")).isEqualTo("last, first");
         assertThat(invert("Mrs. first last")).isEqualTo("last, first");
+        assertThat(invert("first last SR.")).isEqualTo("last, first SR.");
+        assertThat(invert("first last BS. Phd")).isEqualTo("last, first BS. Phd");
     }
 
     private String invert(String name) {
@@ -27,10 +30,17 @@ public class NameInverterTest {
         } else {
             List<String> names = splitNames(name);
             removeHonorific(names);
-            if (names.size() != 2) {
+            if (names.size() == 1) {
                 return names.get(0);
             }
-            return String.format("%s, %s", names.get(1), names.get(0));
+            String postNominal = "";
+            List<String> postNominals = new ArrayList<>();
+            if (names.size() > 2) {
+                postNominals = names.subList(2, names.size());
+            }
+            postNominal = String.join(" ", postNominals);
+
+            return String.format("%s, %s %s", names.get(1), names.get(0), postNominal).trim();
         }
     }
 
